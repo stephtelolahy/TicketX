bda67d079733d1ea4910438a33c74ab33e2cf552
Stéphano TELOLAHY <hugues.stephano.telolahy@devoteam.com>
2022-10-04 22:06:56 +0200

    Initial commit

---
+++ TicketX/TicketXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketXApp.swift
+//  TicketX
+//
+//  Created by TELOLAHY Hugues Stéphano on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicketXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
